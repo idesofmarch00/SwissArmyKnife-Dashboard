261b7fc1846f772eb84950baf74b5568bcb1addc
Sahil Ahmed <sahilahmed@Sahils-MacBook-Pro.local>
2024-07-07 01:56:06 +0530

    remove api key from git

--- js/dashboard/weather.js
+++ js/dashboard/weather.js
@@ -1,64 +1,7 @@
-// const weather = document.querySelector(".js-weather");
-
-// const API_KEY = "5b6ef3caa38dfdec0ef471202470c402";
-// const COORDS = "coords";
-
-// function getWeather(lat, lng) {
-//   fetch(
-//     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
-//   )
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(function(json) {
-//       const temperature = json.main.temp;
-//       const place = json.name;
-//       weather.innerText = `temperature : ${temperature}˚ C
-//       location : ${place}`;
-//     });
-// }
-
-// function saveCoords(coordsObj) {
-//   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
-// }
-
-// function handleGeoSucces(position) {
-//   const latitude = position.coords.latitude;
-//   const longitude = position.coords.longitude;
-//   const coordsObj = {
-//     latitude,
-//     longitude
-//   };
-//   saveCoords(coordsObj);
-//   getWeather(latitude, longitude);
-// }
-
-// function handleGeoError() {
-//   console.log("Can't access geolocation");
-// }
-
-// function askForCoords() {
-//   navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
-// }
-
-// function loadCoords() {
-//   const loadedCoords = localStorage.getItem(COORDS);
-//   if (loadedCoords === null) {
-//     askForCoords();
-//   } else {
-//     const parsedCoords = JSON.parse(loadedCoords);
-//     getWeather(parsedCoords.latitude, parsedCoords.longitude);
-//   }
-// }
-
-// function init() {
-//   loadCoords();
-// }
-
-// init();
+import { config } from "../../env.js";
 
 const weather = document.getElementById("weather");
-const API_KEY = "5b6ef3caa38dfdec0ef471202470c402";
+const API_KEY = config.WEATHER_API_KEY;
 
 navigator.geolocation.getCurrentPosition((position) => {
   fetch(
