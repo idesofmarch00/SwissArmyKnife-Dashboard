370ee01bf421f0e1fdcd0f1ece52c56f794525ac
Sahil Ahmed <sahilahmed@Sahils-MacBook-Pro.local>
2024-07-10 00:21:47 +0530

    Add doc to weather.js

--- js/dashboard/weather.js
+++ js/dashboard/weather.js
@@ -1,8 +1,18 @@
 import { config } from "../../env.js";
 
+/**
+ * DOM element to display the weather information
+ */
 const weather = document.getElementById("weather");
+
+/**
+ * API key for accessing the OpenWeatherMap API
+ */
 const API_KEY = config.WEATHER_API_KEY;
 
+/**
+ * Function to fetch and display the weather information based on the user's current location
+ */
 navigator.geolocation.getCurrentPosition((position) => {
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${API_KEY}&units=metric`
@@ -20,7 +30,7 @@ navigator.geolocation.getCurrentPosition((position) => {
 
       weather.innerHTML = `
                     <div class="weather-header">
-                        <img id="weatherIcon"src=${iconURL} >
+                        <img id="weatherIcon" src=${iconURL} >
                         <p class="weather-temperature">${temperature}&degC</p>
                     </div>
                     <p class="weather-city">${city}</p>
