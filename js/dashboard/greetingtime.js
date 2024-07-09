4c921ec9ce4e6c7361bf998ba8ff9c9c46063ef1
Sahil Ahmed <sahilahmed@Sahils-MacBook-Pro.local>
2024-07-10 00:05:01 +0530

    duck duck go search and time greeting code to be added

--- js/dashboard/greetingtime.js
+++ js/dashboard/greetingtime.js
@@ -1,8 +1,16 @@
+/**
+ * Function to display the current time and a greeting message based on the time of day.
+ */
 function GreetingTime() {
-  var today = new Date();
+  // Get the current date and time
+  const today = new Date();
 
+  // Get the DOM element to display the current time
   const currentTimeEl = document.getElementById("currentTimeEl");
 
+  /**
+   * Function to get the current time and update the DOM element.
+   */
   function getCurrentTime() {
     const date = new Date();
     const currentTime = date.toLocaleTimeString("en-US", {
@@ -11,15 +19,19 @@ function GreetingTime() {
     currentTimeEl.textContent = currentTime;
   }
 
+  // Update the current time every 10 milliseconds
   setInterval(getCurrentTime, 10);
 
+  // Define greeting messages
   const greet1 = "Good Morning,Sahil";
   const greet2 = "Good Afternoon,Sahil";
   const greet3 = "Good Evening,Sahil";
   const greet4 = "Good Night,Sahil";
 
-  var greeting = document.getElementById("greetingHeading");
+  // Get the DOM element to display the greeting message
+  const greeting = document.getElementById("greetingHeading");
 
+  // Determine the appropriate greeting message based on the current hour
   if (today.getHours() > 5 && today.getHours() < 12) {
     greeting.innerHTML = greet1;
   }
@@ -33,4 +45,6 @@ function GreetingTime() {
     greeting.innerHTML = greet4;
   }
 }
+
+// Update the greeting message every 10 milliseconds
 setInterval(GreetingTime, 10);
